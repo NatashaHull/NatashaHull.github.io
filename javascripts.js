57f0027be641f67b8a5c02c27218b6584fd0bf40
Natasha Hull <musicalnat@gmail.com>
2013-11-26 20:48:33 -0800

    fixed a typo

--- javascripts.js
+++ javascripts.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     "I've written countless <strong>Rails</strong> applications, and wrote <a href='#rails-lite'>a mini-version of Rails</a> using WEBrick.",
     "I've written my share of nasty joins in <strong>SQL</strong>, and I wrote <a href='#active-record-lite'>my own ORM</a> for Rails.",
     "I'm proficient in <strong>JavaScript</strong>, jQuery, and Backbone.",
-    "I picked up <strong>Python</strong> last summer while self-teaching myself using Berkeley's AI class for fun.",
+    "I picked up <strong>Python</strong> last summer while teaching myself using Berkeley's AI class for fun.",
     "Design isn't as fun as programming for me, but I know enough <strong>CSS</strong> to get by.",
     "I've built a lot of small <a target='_blank' href='https://github.com/NatashaHull/AppAcademyProjects/tree/master/Week6'>JavaScript games</a> using <strong>jQuery</strong>.",
     "I have converted <a href='#hack-underflow'>a complex Rails app</a> into a client-side <strong>Backbone.js</strong> app.",
